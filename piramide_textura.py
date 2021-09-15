94242226d40a701ebddc687e95388adf17782e36
lps08 <lucianolps08@gmail.com>
2021-09-15 17:13:02 -0300

    piramide com textura

--- piramide_textura.py
+++ piramide_textura.py
@@ -4,38 +4,9 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-import sys
-
-vertices = (
-    # x  y  z
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1)
-)
-
-edges = (
-    (0, 1),
-    (0, 3),
-    (0, 4),
-    (2, 1),
-    (2, 3),
-    (2, 7),
-    (6, 3),
-    (6, 4),
-    (6, 7),
-    (5, 1),
-    (5, 4),
-    (5, 7)
-)
-
 
 def loadTexture():
-    textureSurface = pygame.image.load('texture.jpg')
+    textureSurface = pygame.image.load('textura.png')
     textureData = pygame.image.tostring(textureSurface, "RGBA", 1)
     width = textureSurface.get_width()
     height = textureSurface.get_height()
@@ -54,86 +25,103 @@ def loadTexture():
 
     return texid
 
+def piramide():
+    glBegin(GL_QUADS)
 
-def draw_cube(lines=False):
-    if lines:
-        glBegin(GL_LINES)
-        for edge in edges:
-            glColor3fv((1, 1, 1))
-            for vertex in edge:
-                glVertex3fv(vertices[vertex])
-        glEnd()
-    else:
-        glBegin(GL_QUADS)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(-1.0, -1.0,  1.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(1.0, -1.0,  1.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(1.0,  1.0,  1.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(-1.0,  1.0,  1.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(-1.0, -1.0, -1.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(-1.0,  1.0, -1.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(1.0,  1.0, -1.0)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(1.0, -1.0, -1.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(-1.0,  1.0, -1.0)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(-1.0,  1.0,  1.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(1.0,  1.0,  1.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(1.0,  1.0, -1.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(-1.0, -1.0, -1.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(1.0, -1.0, -1.0)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(1.0, -1.0,  1.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(-1.0, -1.0,  1.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(1.0, -1.0, -1.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(1.0,  1.0, -1.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(1.0,  1.0,  1.0)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(1.0, -1.0,  1.0)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(-1.0, -1.0, -1.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(-1.0, -1.0,  1.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(-1.0,  1.0,  1.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(-1.0,  1.0, -1.0)
-        glEnd()
+    glTexCoord2f(0.0, 0.0)
+    glVertex3f(0, 0, 1)
+    
+    glTexCoord2f(1.0, 0.0)
+    glVertex3f(1, -1, -1)
 
-pygame.init()
-display = (800, 600)
-pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(0, 0, 1)
 
-loadTexture()
+    glTexCoord2f(0.0, 1.0)
+    glVertex3f(1, 1, -1)
 
-gluPerspective(45, display[0] / display[1], 0.1, 50.0)
-glTranslatef(0.0, 0.0, -5)
+    glTexCoord2f(1.0, 0.0)
+    glVertex3f(0, 0, 1)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(-1, 0, -1)
 
-    glRotatef(1, 1, 1, 1)
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glTexCoord2f(0.0, 1.0)
+    glVertex3f(1, -1, -1)
 
-    draw_cube(lines=False)
+    glTexCoord2f(0.0, 0.0)
+    glVertex3f(1, 1, -1)
 
-    pygame.display.flip()
-    pygame.time.wait(10)
+    glTexCoord2f(1.0, 0.0)
+    glVertex3f(1, -1, -1)
+
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(1, 1, -1)
+
+    glTexCoord2f(0.0, 1.0)
+    glVertex3f(1, -1, -1)
+
+    glTexCoord2f(0.0, 1.0)
+    glVertex3f(-1, 0, -1)
+
+    glTexCoord2f(0.0, 0.0)
+    glVertex3f(1, -1, -1)
+
+    glTexCoord2f(1.0, 0.0)
+    glVertex3f(-1, 0, -1)
+
+    glTexCoord2f(1.0, 0.0)
+    glVertex3f(1, 1, -1)
+
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(-1, 0, -1)
+    
+    glEnd()
+
+def main():
+    """
+        Função principal onde irá renderizar a construção do objeto
+    """
+    # iniciando o pygame, onde é o responsável pela renderização
+    pygame.init()
+
+    # definindo o tamanho do display da janaela de visualização
+    display = (800, 600)
+    # aplicando o tamanho da tela e definindo as configurações necessárias do opengl
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    loadTexture()
+
+    # adicionando a perspectiva 
+    gluPerspective(40, (display[0]/display[1]), 0.1, 30.0)
+
+    # adicionanodo a translação
+    glTranslatef(0.0, -0.5, -20)
+
+    # rotacionando o objeto inicial
+    glRotatef(30, 2, 1, 2)
+
+    # usando a escala no objeto
+    glScale(4, 4, 4)
+
+    # loop que faz a renderização do objeto utilizando o pygame
+    while True:
+        for event in pygame.event.get():
+            # se o usuario fechar a janela
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        # para cada frame, o objeto rataciona
+        glRotatef(1, 1, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+        # chamando o objeto criado
+        piramide()
+
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+# inicializando a função principal
+if __name__ == '__main__':
+    main()
