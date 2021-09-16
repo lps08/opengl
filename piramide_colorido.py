d2913297894d701b1cb20a9c2e8f7b91211d8e11
lps08 <lucianolps08@gmail.com>
2021-09-16 10:41:59 -0300

    descoment

--- piramide_colorido.py
+++ piramide_colorido.py
@@ -106,14 +106,14 @@ def main():
     glMatrixMode(GL_MODELVIEW)
     glTranslatef(0.0, 0, -5)
 
-    glRotatef(-50, 10, -10, 1)
+    # glRotatef(-50, 10, -10, 1)
     # parametros de iluminação
-    # glLight(GL_LIGHT0, GL_POSITION,  (0, 0, 1, 0))
-    # glLight(GL_LIGHT0, GL_POSITION,  (5, 5, 5, 1))
-    # glLightfv(GL_LIGHT0, GL_AMBIENT, (0, 0, 0, 1))
-    # glLightfv(GL_LIGHT0, GL_DIFFUSE, (1, 1, 1, 1))
+    glLight(GL_LIGHT0, GL_POSITION,  (0, 0, 1, 0))
+    glLight(GL_LIGHT0, GL_POSITION,  (5, 5, 5, 1))
+    glLightfv(GL_LIGHT0, GL_AMBIENT, (0, 0, 0, 1))
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, (1, 1, 1, 1))
 
-    # glEnable(GL_DEPTH_TEST)
+    glEnable(GL_DEPTH_TEST)
 
     # loop que faz a renderização do objeto utilizando o pygame
     while True:
@@ -126,11 +126,11 @@ def main():
         # glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
         # habilitação da iluminação
-        # glEnable(GL_LIGHTING)
-        # glEnable(GL_LIGHT0)
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
 
-        # glEnable(GL_COLOR_MATERIAL)
-        # glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE )
+        glEnable(GL_COLOR_MATERIAL)
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE )
 
         # glRotatef(1, 3, 1, 1)
         # chamando o objeto criado
